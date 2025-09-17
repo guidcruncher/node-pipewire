0dd6721aa7bfdbf0ef10c616236068e32601d132
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-17 02:52:37 +0000

    issue with icecadt

--- src/icecast.sh
+++ src/icecast.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-
-icecast -c /local/config/icecast/icecast.xml
+if [ "$1" == "shutdown" ]; then
+  trap 'echo ' ERR SIGINT SIGTERM
+  kill $(pgrep -f ffmpeg)
+  kill $(pgrep -f icecast)
+else
+  icecast -c /local/config/icecast/icecast.xml
+fi

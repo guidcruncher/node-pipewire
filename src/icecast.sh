c4c92a264dd2bcde37cf3ee5309f176c74263daa
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-17 05:18:00 +0000

    fixs

--- src/icecast.sh
+++ src/icecast.sh
@@ -2,8 +2,14 @@
 
 if [ "$1" == "shutdown" ]; then
   trap 'echo ' ERR SIGINT SIGTERM
-  kill $(pgrep -f ffmpeg)
-  kill $(pgrep -f icecast)
+  pid=$(pgrep -f ffmpeg)
+  if [ -n "$pid" ]; then
+    kill $pid
+  fi
+  pid=$(pgrep -f icecast)
+  if [ -n "$pid" ]; then
+    kill $pid
+  fi
 else
   icecast -c /local/config/icecast/icecast.xml
 fi

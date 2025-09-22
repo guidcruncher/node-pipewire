74dcb8192206ee55e422e0c53165fd22d90e93cb
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-22 03:13:08 +0000

    tests

--- src/snapserver.sh
+++ src/snapserver.sh
@@ -6,6 +6,10 @@ if [ "$1" == "shutdown" ]; then
   if [ -n "$pid" ]; then
     kill $pid
   fi
+  pid=$(pgrep -f snapclient)
+  if [ -n "$pid" ]; then
+    kill $pid
+  fi
 else
   for file in /local/.defaults/snapserver/*.conf
   do
@@ -13,5 +17,7 @@ else
     envsubst < "$file" > /local/config/snapserver/$(basename "$file")
   done
 
-  /usr/bin/snapserver -c /local/config/snapserver/snapserver.conf
+  /usr/bin/snapserver -d -c /local/config/snapserver/snapserver.conf
+  sleep 2
+  /usr/bin/snapclient --player alsa -s soundcard  --host 127.0.0.1
 fi

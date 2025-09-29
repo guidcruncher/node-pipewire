903bbeb3dcf323b5029e02278627207427021e82
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-29 10:27:31 +0000

    Rollback

--- src/pipewire-launcher.sh
+++ src/pipewire-launcher.sh
@@ -22,9 +22,3 @@ fi
 
 [ -f "$XDG_CONFIG_HOME/pipewire/pipewire-pulse.conf" ] && exec /usr/bin/pipewire-pulse -c $XDG_CONFIG_HOME/pipewire/pipewire-pulse.conf&
 
-pkill Xvfb
-sleep 1
-Xvfb :43 -screen 0 1024x768x16 &
-sleep 3
-export DISPLAY=:43
-easyeffects --gapplication-service &

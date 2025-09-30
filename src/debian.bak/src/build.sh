75250e25a20019252a26a9c8ff0dd6798d039cd7
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-09-30 14:56:19 +0000

    Removed Debian Old POC

--- src/debian.bak/src/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker buildx build ./src/ -f ./src/Dockerfile -t guidcruncher/node-pipewire:debian-latest --pull --push
--- src/debian.bak/src/scripts/pm2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PM2_HOME="$PM2_CONTAINER_HOME"
-
-pm2 $@
-
-export PM2_HOME="$PM2_CONTAINER_HOME"
